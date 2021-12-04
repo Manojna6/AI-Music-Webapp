3485a05fcd7f0cc0e10fa6f536a2624b24c165e9
Manojna6 <72754918+Manojna6@users.noreply.github.com>
2021-12-04 11:56:34 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+lx = 0;
+ly = 0;
+rx = 0;
+ry = 0;
 
 function preload() {
     song1 = loadSound("music.mp3");
@@ -10,7 +14,21 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses)
 }
 function draw() {
     image(video, 0, 0, 600, 500);
+}
+function modelLoaded() {
+    console.log("model loaded")
+}
+function gotPoses(results) {
+    if(results != 0){
+        console.log(results)
+        lx = results[0].pose.leftWrist.x;
+        ly = results[0].pose.leftWrist.y;
+        rx = results[0].pose.rightWrist.x;
+        ry = results[0].pose.rightWrist.y;
+    }
 }
